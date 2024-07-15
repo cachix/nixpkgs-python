04805b06f79197e7a3bd302bd76aaaa1c43c7d8a
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2024-07-15 08:41:19 +0200

    Add ActiveState Python 2.7

--- update.py
+++ update.py
@@ -4,6 +4,7 @@ import base64
 import hashlib
 
 source = lambda version: f"https://www.python.org/ftp/python/{version}/Python-{version}.tgz"
+activestate_source = lambda version: f"https://github.com/ActiveState/cpython/archive/refs/tags/v{version}.tar.gz"
 
 # open versions.json and parse json
 def get_versions():
@@ -58,6 +59,30 @@ def get_all_releases(response, versions):
                 }
     return versions
 
+def get_activestate_releases(response, versions):
+    for entry in response.json():
+        version = entry["tag_name"].lstrip("v")
+        cycle = '.'.join(version.split('.')[:2])
+        release = versions['releases'].get(version, {})
+
+        if cycle != "2.7":
+            continue
+
+        versions['latest'][cycle] = max(version, versions['latest'].get(cycle, ''))
+
+        if release.get("hash"):
+            continue
+        else:
+            url = activestate_source(version)
+            print(f'Downloading {url}')
+            response = requests.get(url)
+            response.raise_for_status()
+            versions['releases'][version] = {
+                "hash": calculate_sha256(response.content),
+                "url": url
+            }
+    return versions
+
 def calculate_sha256(contents):
     return base64.b64encode(hashlib.sha256(contents).digest()).decode('utf-8')
 
@@ -68,5 +93,9 @@ if __name__ == "__main__":
     response = requests.get("https://endoflife.date/api/python.json")
 
     versions = get_all_releases(response, versions)
+
+    activestate_response = requests.get("https://api.github.com/repos/ActiveState/cpython/releases")
+    versions = get_activestate_releases(activestate_response, versions)
+
     with open("versions.json", "w") as f:
         json.dump(versions, f, indent=4)
