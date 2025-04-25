0198cafa5bd313e069ef9d81d1e860184eb35cfd
Sander <hey@sandydoo.me>
2025-04-25 13:26:58 +0400

    use extra-substituters

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   };
 
   nixConfig = {
-    substituters = "https://cache.nixos.org https://nixpkgs-python.cachix.org";
+    extra-substituters = "https://nixpkgs-python.cachix.org";
     extra-trusted-public-keys = "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU=";
   };
 
