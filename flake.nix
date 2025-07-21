ff8227790b001dc20d40d0c8eda5263b0386f4ab
Sander <hey@sandydoo.me>
2025-07-21 16:47:22 +0200

    deps: upgrade nixpkgs to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "All Python versions packages in Nix.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-compat.url = "github:edolstra/flake-compat";
     flake-compat.flake = false;
   };
